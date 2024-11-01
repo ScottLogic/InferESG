cbb764459f90e5b86210562d801e90ac9f3e9260
Ivan Mladjenovic (He/Him) <imladjenovic@scottlogic.com>
2024-11-01 11:26:44 +0000

    Move promptfoo to its own folder in /backend. Load OpenAI key from env file. Simplify test structure so less utility .py classes are needed

--- backend/promtfoo/prompt_foo_runner.py
+++ backend/promtfoo/prompt_foo_runner.py
@@ -1,9 +1,16 @@
-from prompting import PromptEngine
+import sys
+import os
+sys.path.append("../")
+from dotenv import load_dotenv, find_dotenv  # noqa: E402
+from src.prompts.prompting import PromptEngine  # noqa: E402
 
+load_dotenv(find_dotenv())
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 engine = PromptEngine()
 
 
-def get_prompt(context):
+def generate_message_suggestions(context):
     chat_history = context["vars"]["chatHistory"]
 
     system_prompt = engine.load_prompt("generate_message_suggestions", chat_history=chat_history)
