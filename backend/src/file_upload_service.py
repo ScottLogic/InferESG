fb5740d73a48a2ca45df42431666ca66170b9c3e
Mike Smith <mic-smith@users.noreply.github.com>
2024-11-06 15:05:18 +0000

    FS-82 Add file upload api

---
+++ backend/src/file_upload_service.py
@@ -0,0 +1,63 @@
+from io import TextIOWrapper
+import time
+from fastapi import HTTPException, UploadFile
+import logging
+import uuid
+
+from pypdf import PdfReader
+from src.session.file_uploads import FileUpload, update_session_file_uploads, get_session_file_upload
+
+logger = logging.getLogger(__name__)
+
+MAX_FILE_SIZE = 10*1024*1024
+
+def handle_file_upload(file:UploadFile) -> str:
+
+    if (file.size or 0) > MAX_FILE_SIZE:
+        raise HTTPException(status_code=400, detail=f"File upload must be less than {MAX_FILE_SIZE} bytes")
+
+
+    all_content = ""
+    if ("application/pdf" == file.content_type):
+
+        start_time = time.time()
+        pdf_file = PdfReader(file.file)
+        all_content = ""
+        for page_num in range(len(pdf_file.pages)):
+            page_text = pdf_file.pages[page_num].extract_text()
+            all_content += page_text
+            all_content += "\n"
+
+        # pdfminer.six processing
+        # out_str = StringIO()
+        # extract_text_to_fp(file.file, out_str) #, codec='cp1251', strip_control=True)
+        # all_content = out_str.getvalue()
+
+        end_time = time.time()
+
+        logger.debug(f'PDF content {all_content}')
+        logger.info(f"PDF content extracted successfully in {(end_time - start_time)}")
+
+
+    elif ("text/plain" == file.content_type):
+        all_content = TextIOWrapper(file.file, encoding='utf-8').read()
+        logger.debug(f'Text content {all_content}')
+    else:
+        raise HTTPException(status_code=400,
+                            detail="File upload must be supported type (text/plain or application/pdf)")
+
+    session_file = FileUpload(uploadId=str(uuid.uuid4()),
+                             contentType=file.content_type or "" ,
+                             filename=file.filename or "",
+                             content=all_content or "",
+                             size=file.size or 0)
+
+    update_session_file_uploads(session_file)
+
+    return session_file["uploadId"]
+
+def get_file_upload(upload_id) -> FileUpload | None:
+    return get_session_file_upload(upload_id)
+
+
+
