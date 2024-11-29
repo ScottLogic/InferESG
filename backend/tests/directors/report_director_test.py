cef4e0bdefb3ee1cc174d1e23362a742c6e6ae43
Ivan Mladjenovic (He/Him) <imladjenovic@scottlogic.com>
2024-11-29 15:55:34 +0000

    attempt to fix tests again

--- backend/tests/directors/report_director_test.py
+++ backend/tests/directors/report_director_test.py
@@ -1,6 +1,7 @@
 from io import BytesIO
 from fastapi import UploadFile
 from fastapi.datastructures import Headers
+from src.llm import MockLLM
 import pytest
 
 from src.session.file_uploads import FileUpload
@@ -12,10 +13,8 @@ async def test_report_on_file_upload(mocker):
 
     file_upload = FileUpload(uploadId="1", filename="test.txt", content="test", contentType="text/plain", size=4)
 
-    mock_handle_file_upload = mocker.patch(
-        "src.directors.report_director.report_on_file_upload",
-        return_value=file_upload
-    )
+    mock_handle_file_upload = mocker.patch("src.directors.report_director.handle_file_upload", return_value=file_upload)
+    mock_llm = mocker.patch("src.agents.get_llm", return_value=MockLLM())
 
     headers = Headers({"content-type": "text/plain"})
     file = BytesIO(b"test content")
@@ -23,4 +22,5 @@ async def test_report_on_file_upload(mocker):
     response = await report_on_file_upload(request_upload_file)
 
     mock_handle_file_upload.assert_called_once_with(request_upload_file)
-    assert response ==  {"filename": "test.txt", "id": "1", "report": "#Report on upload as markdown"}
+    mock_llm.assert_called_once_with("test content")
+    assert response == {"filename": "test.txt", "id": "1", "report": "#Report on upload as markdown"}
