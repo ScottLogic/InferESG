c807e0bb417b63ca1bab89a0328952326b938ec8
Ivan Mladjenovic (He/Him) <imladjenovic@scottlogic.com>
2024-12-05 15:03:33 +0000

    first pass

--- backend/src/llm/openai.py
+++ backend/src/llm/openai.py
@@ -1,18 +1,24 @@
 # src/llm/openai_llm.py
 import logging
-from .openai_client import OpenAIClient
+from typing import Optional
+from dataclasses import dataclass
+
 from src.utils import Config
-from .llm import LLM
+from src.llm import LLM, LLMChatWithFile
 from openai import NOT_GIVEN, AsyncOpenAI
 
 logger = logging.getLogger(__name__)
 config = Config()
 
 
-class OpenAI(LLM):
-    def __init__(self):
-        self.client = OpenAIClient()
+@dataclass
+class LLMFile:
+    file_name: str
+    file_path: Optional[str] = None  # Materiality docs will be local
+    file_stream: Optional[bytes] = None  # report agent file will be a stream. Not sure bytes is correct, whatever api/app.py is working with
 
+
+class OpenAI(LLM):
     async def chat(self, model, system_prompt: str, user_prompt: str, return_json=False) -> str:
         logger.debug(
             "##### Called open ai chat ... llm. Waiting on response model with prompt {0}.".format(
@@ -44,3 +50,56 @@ class OpenAI(LLM):
         except Exception as e:
             logger.error(f"Error calling OpenAI model: {e}")
             return "An error occurred while processing the request."
+
+
+class OpenAIChatWithFile(LLMChatWithFile):
+    async def chat_with_file(
+        self,
+        model: str,
+        system_prompt: str,
+        user_prompt: str,
+        files: list[LLMFile]
+    ) -> str:
+        client = AsyncOpenAI(api_key=config.openai_key)
+
+        file_ids = []
+        for file in files:
+            # check in Redis if this file exists
+                # if it doesn't exist, upload it and save the file_id
+                    # if LLMFile has path to upload use upload_file(file_path)
+                    # if LLMFile has stream then upload stream
+                # if it does exist then redis will have the file id uploaded to openai. fetch it
+            file_ids.append("id from logic in pseudo code above")
+
+        file_assistant = await client.beta.assistants.create(
+            name="ESG Analyst",
+            instructions=system_prompt,
+            model=model,
+            tools=[{"type": "file_search"}],
+        )
+
+        thread = await client.beta.threads.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": user_prompt,
+                    "attachments": [
+                        {"file_id": file_id, "tools": [{"type": "file_search"}]}
+                        for file_id in file_ids
+                    ],
+                }
+            ]
+        )
+
+        messages = await client.beta.threads.runs.create_and_poll(
+            thread_id=thread.id, assistant_id=file_assistant.id
+        )
+
+        logger.info(messages.data[0])
+        return messages.data[0]  # haven't gotten around to checking how to get the content out yet.
+
+
+async def upload_file(file_path: str) -> str:
+    client = AsyncOpenAI(api_key=config.openai_key)
+    file = await client.files.create(file=open(file_path, "rb"), purpose="assistants")
+    return file.id
