5df7722b87e72ca10d932824dc4ad6e3278114f1
Ivan Mladjenovic (He/Him) <imladjenovic@scottlogic.com>
2024-12-05 15:03:33 +0000

    first pass

--- backend/src/llm/llm.py
+++ backend/src/llm/llm.py
@@ -3,6 +3,9 @@ from typing import Any, Coroutine
 from .count_calls import count_calls
 
 
+count_calls_of_functions = ["chat", "chat_with_file"]
+
+
 class LLMMeta(ABCMeta):
     def __init__(cls, name, bases, namespace):
         super().__init__(name, bases, namespace)
@@ -12,8 +15,9 @@ class LLMMeta(ABCMeta):
         cls.instances[name.lower()] = cls()
 
     def __new__(cls, name, bases, attrs):
-        if "chat" in attrs:
-            attrs["chat"] = count_calls(attrs["chat"])
+        for function in count_calls_of_functions:
+            if function in attrs:
+                attrs[function] = count_calls(attrs[function])
 
         return super().__new__(cls, name, bases, attrs)
 
@@ -25,4 +29,20 @@ class LLM(ABC, metaclass=LLMMeta):
 
     @abstractmethod
     def chat(self, model: str, system_prompt: str, user_prompt: str, return_json=False) -> Coroutine[Any, Any, str]:
+        pass
+
+
+class LLMChatWithFile(ABC, metaclass=LLMMeta):
+    @classmethod
+    def get_instances(cls):
+        return cls.instances
+
+    @abstractmethod
+    def chat_with_file(
+        self,
+        model: str,
+        system_prompt: str,
+        user_prompt: str,
+        file_paths: list[str]
+    ) -> Coroutine:
         pass
