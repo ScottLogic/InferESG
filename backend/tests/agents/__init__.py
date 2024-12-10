d5b29b57d594956fb834ff1bfe8f387d2a8ff9e8
Ivan Mladjenovic (He/Him) <imladjenovic@scottlogic.com>
2024-12-10 16:45:14 +0000

    fix pytest

--- backend/tests/agents/__init__.py
+++ backend/tests/agents/__init__.py
@@ -1,4 +1,4 @@
-from src.agents import Agent, agent, tool, Parameter
+from src.agents import ChatAgent, agent, tool, Parameter
 
 name_a = "Mock Tool A"
 name_b = "Mock Tool B"
@@ -38,8 +38,8 @@ mock_tools = [mock_tool_a, mock_tool_b]
 
 
 @agent(name=mock_agent_name, description=mock_agent_description, tools=mock_tools)
-class MockAgent(Agent):
+class MockChatAgent(ChatAgent):
     pass
 
 
-__all__ = ["MockAgent", "mock_agent_description", "mock_agent_name", "mock_tools", "mock_tool_a", "mock_tool_b"]
+__all__ = ["MockChatAgent", "mock_agent_description", "mock_agent_name", "mock_tools", "mock_tool_a", "mock_tool_b"]
