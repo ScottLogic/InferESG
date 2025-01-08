9c99c87bf80c632b9c0e818acab5c99ca31f14a0
Ivan Mladjenovic (He/Him) <imladjenovic@scottlogic.com>
2025-01-08 15:26:27 +0000

    fix router tests

--- backend/tests/router_test.py
+++ backend/tests/router_test.py
@@ -1,14 +1,21 @@
 import json
+from unittest.mock import MagicMock
 
 import pytest
 from tests.llm.mock_llm import MockLLM
-from tests.agents import MockChatAgent
-from src.agents.agent import chat_agent
+from src.agents.agent import chat_agent, ChatAgent
 from src.router import get_agent_for_task
 
 
 mock_model = "mockmodel"
 mock_llm = MockLLM()
+
+
+class MockChatAgent(ChatAgent):
+    async def validate(self, utterance: str, answer: str) -> bool:
+        pass
+
+
 mock_agent_1 = chat_agent("chat_agent_1", "description_1", [])(MockChatAgent("mockllm", mock_model))
 mock_agent_2 = chat_agent("chat_agent_2", "description_2", [])(MockChatAgent("mockllm", mock_model))
 mock_agents = [mock_agent_1, mock_agent_2]
@@ -19,6 +26,7 @@ async def test_get_agent_for_task_no_agent_found(mocker):
     selected_agent = '{"agent_name": "this_agent_does_not_exist"}'
     mocker.patch("src.router.get_llm", return_value=mock_llm)
     mocker.patch("src.router.get_chat_agents", return_value=mock_agents)
+    mocker.patch("src.router.config.router_model", new_callable=MagicMock)
     mock_llm.chat = mocker.AsyncMock(return_value=selected_agent)
 
     agent = await get_agent_for_task("task1", [])
@@ -31,6 +39,7 @@ async def test_get_agent_for_task_agent_found(mocker):
     selected_agent = {"agent_name": mock_agent_1.name}
     mocker.patch("src.router.get_llm", return_value=mock_llm)
     mocker.patch("src.router.get_chat_agents", return_value=mock_agents)
+    mocker.patch("src.router.config.router_model", new_callable=MagicMock)
     mock_llm.chat = mocker.AsyncMock(return_value=json.dumps(selected_agent))
 
     agent = await get_agent_for_task("task1", [])
@@ -43,6 +52,7 @@ async def test_get_agent_for_task_is_not_given_excluded_agents(mocker):
     plan = {"agent_name": mock_agent_2.name}
     mocker.patch("src.router.get_llm", return_value=mock_llm)
     mocker.patch("src.router.get_chat_agents", return_value=mock_agents)
+    mocker.patch("src.router.config.router_model", new_callable=MagicMock)
     mock_llm.chat = mocker.AsyncMock(return_value=json.dumps(plan))
     spy_chat = mocker.spy(mock_llm, 'chat')
 
