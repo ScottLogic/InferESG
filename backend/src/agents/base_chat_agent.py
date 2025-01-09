ae1c9b0534c5b966351af8c598cdbb8e5c8e97ef
Ivan Mladjenovic (He/Him) <imladjenovic@scottlogic.com>
2025-01-09 10:37:39 +0000

    remove TODO and unused variables from base chat agent

--- backend/src/agents/base_chat_agent.py
+++ backend/src/agents/base_chat_agent.py
@@ -1,18 +1,12 @@
-import logging
-
 from src.agents import ChatAgent
 from src.utils import Config
-from src.prompts import PromptEngine
 from src.agents.validator_agent import ValidatorAgent
 
-logger = logging.getLogger(__name__)
-engine = PromptEngine()
 config = Config()
 
 
 class BaseChatAgent(ChatAgent):
     async def validate(self, utterance: str, answer: str) -> bool:
-        # TODO think whether we need a validator agent, or just validator llm config
         validator_agent = ValidatorAgent(config.validator_agent_llm, config.validator_agent_model)
         validation = (await validator_agent.validate(f"Task: {utterance}  Answer: {answer}")).lower() == "true"
         return validation
