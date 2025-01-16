d91e106e3f2cf4b5171b530ea12ecb5b360d6cf4
Ivan Mladjenovic (He/Him) <imladjenovic@scottlogic.com>
2025-01-16 14:30:27 +0000

    PR feedback

--- backend/src/agents/adapters.py
+++ backend/src/agents/adapters.py
@@ -14,15 +14,15 @@ def extract_tool(tool_name: str, agent_tools: List[Tool], parameters: dict[str, 
 
 
 def validate_args(tool: Tool, parameters: dict[str, Any]):
-    all_params = set(tool.parameters.keys())
-    required_params = {key for key, param in tool.parameters.items() if param.required}
+    tool_params = set(tool.parameters.keys())
+    tool_required_params = {key for key, param in tool.parameters.items() if param.required}
     input_params = set(parameters.keys())
 
-    unknown_parameters = input_params - all_params
-    missing_parameters = required_params - input_params
+    unknown_input_parameters = input_params - tool_params
+    missing_input_parameters = tool_required_params - input_params
 
-    if missing_parameters or unknown_parameters:
+    if missing_input_parameters or unknown_input_parameters:
         raise Exception(
-            f"Tool {tool.name} has parameters {all_params}. Tool was called with {input_params}. "
-            f"Missing Parameters: {missing_parameters}. Unknown Parameters: {unknown_parameters}."
+            f"Tool {tool.name} has parameters {tool_params}. Tool was called with {input_params}. "
+            f"Missing Parameters: {missing_input_parameters}. Unknown Parameters: {unknown_input_parameters}."
         )
